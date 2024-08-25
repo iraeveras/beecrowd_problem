6a651f70d1dfca4eb1827366e33737975889b8c6
iraeveras <irae.online@gmail.com>
2024-08-25 19:36:46 -0300

    docs: adcionado o enunciado da questão

--- problem/1003.js
+++ problem/1003.js
@@ -1,8 +1,29 @@
+/*
+Leia dois valores inteiros, no caso para variáveis A e B. 
+A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido 
+pelo valor correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim de linha após o 
+resultado, caso contrário, você receberá "Presentation Error".
+
+    EXEMPLO DE ENTRADA | EXEMPLO DE SAIDA
+    30                 |  SOMA = 40
+    10                 |
+    -------------------------------------
+    -30                |  SOMA = -20
+    10                 |
+    -------------------------------------
+    0                  |  SOMA = 0
+    0                  |
+
+*/
 export function problem (lines) {
-    const A = Number(lines[0]);
-    const B = Number(lines[1]);
+    const soma = (a, b) => a + b;
+    const resultado = lines.map(Number).reduce(soma);
 
-    const soma = A + B;
-
-    console.log(`SOMA = ${soma}`);    
+    console.log(`SOMA = ${resultado}`);    
 }
