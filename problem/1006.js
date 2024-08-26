8aa85dcc044d784b5ab5aad420e4ebc7e6f36c63
iraeveras <irae.online@gmail.com>
2024-08-25 21:10:03 -0300

    feat: cabeçalho da questão

--- problem/1006.js
+++ problem/1006.js
@@ -9,7 +9,7 @@ O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão 
 Saída
 Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. 
 Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
-
+    EXEMPLO DE ENTRADA | EXEMPLO DE SAIDA
     5.0                |  MEDIA = 6.3
     6.0                |
     7.0                |
